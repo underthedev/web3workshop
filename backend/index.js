e2b8771e493c1377640288d794594c336b6af086
UNDER THE DEV <joker4underthedev@gmail.com>
2022-04-23 19:06:37 +0700

    Add ethers

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const { ethers, utils } = require('ethers');
+const fs = require('fs');
+const { join } = require('path');
 const app = express();
+require('dotenv').config()
 
-app.get("/", (req, res) => {
-    res.send("hello")
+app.get("/", async (req, res) => {
+    const rawJson = fs.readFileSync(join('./', 'abi.json'), 'utf8');
+    const abi = JSON.parse(rawJson);
+    const provider = new ethers.providers.JsonRpcProvider(`${process.env.KOVAN_URL}`, 42);
+
+    const contract = new ethers.Contract(
+        `${process.env.CONTRACT_ADDRESS}`,
+        abi,
+        provider);
+    
+    const goal = await contract.goal();
+    
+    res.send(utils.formatEther(goal));
 })
 
 app.listen(5000, () => {
-    console.log("Running on port 5000.")
+    console.log("Running on port 5000.");
 })
 
 module.export = app;
