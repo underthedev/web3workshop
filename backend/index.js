51fa5ed8344e6f5a7fa38d015ae1226731071ae2
UNDER THE DEV <joker4underthedev@gmail.com>
2022-04-23 19:59:15 +0700

    Final backend

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,22 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-
-app.get("/", async (req, res) => {
+app.use((req, res, next) => {
     const rawJson = fs.readFileSync(join('./', 'abi.json'), 'utf8');
     const abi = JSON.parse(rawJson);
     const provider = new ethers.providers.JsonRpcProvider(`${process.env.KOVAN_URL}`, 42);
-
+    const signer = new ethers.Wallet(`${process.env.PRIVATE_KEY}`, provider)
     const contract = new ethers.Contract(
         `${process.env.CONTRACT_ADDRESS}`,
         abi,
-        provider);
+        signer);
+
+    req.contract = contract;
+    next();
+})
+
+app.get("/", async (req, res) => {
+    const contract = req.contract;
     
     const goalTask = contract.goal().then(res => utils.formatEther(res));
     const poolTask = contract.pool().then(res => utils.formatEther(res));
@@ -26,6 +32,18 @@ app.get("/", async (req, res) => {
     const progress = ((pool / goal) * 100).toPrecision(4);
 
     res.json({goal, pool, endTime, progress});
+})
+
+app.get("/pause", async (req, res) => {
+    const contract = req.contract;
+    const tx = await contract.pause();
+    res.json({txHash: tx.hash});
+})
+
+app.get("/unpause", async (req, res) => {
+    const contract = req.contract;
+    const tx = await contract.unpause();
+    res.json({txHash: tx.hash});
 })
 
 app.listen(5000, () => {
